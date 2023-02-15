ff89e254292f07e37f90a2c49ac486978c2c1b7a
Daniel Rodriguez <jdrs1998@gmail.com>
2023-02-14 19:35:56 -0500

    was made the introduction in the page with some images

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,8 +1,61 @@
 const Introduction = () => {
-    return ( <div className="flex justify-center h-full relative">
-        <div className="rounded ... h-80 w-4/5 content-center justoify-center
+    return ( <div className="flex justify-center h-full w-full relative">
+        <div className="rounded ... h-110 w-4/5 content-center justify-center
         my-20 backdrop-blur-[50px] ">
-            <h2>Esta es la introduccion</h2>
+            <h2>Ofrecemos soluciones para las diferentes areas</h2>
+
+            <div className="w-full h-full flex flex-wrap justify-center">
+            <div className="w-3/12 h-2/4 bg-red-900 m-2">
+            <div>
+            <img className="object-cover "
+            alt="cience" 
+            src="https://images.pexels.com/photos/2280547/pexels-photo-2280547.jpeg?auto=compress&cs=tinysrgb&w=400"></img>
+            </div>
+            <h2 className="text-white">Ciencias</h2>   
+            </div>
+            <div className="w-3/12 h-2/4 bg-red-900 m-2">
+            <div>
+            <img className="object-fill"
+            alt="cience" 
+            src="https://images.pexels.com/photos/5184957/pexels-photo-5184957.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+            </div>
+            <h2>Calculo</h2>
+            </div>
+            <div className="w-3/12 h-2/4 bg-red-900 m-2">
+            <div>
+            <img className="object-fill"
+            alt="cience" 
+            src="https://images.pexels.com/photos/6930902/pexels-photo-6930902.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+            </div>
+            <h2>Estadistica</h2>
+            </div>
+            <div className="w-3/12 h-2/4 bg-red-900 m-2">
+            <div>
+            <img className="object-fill"
+            alt="cience" 
+            src="https://images.pexels.com/photos/8472938/pexels-photo-8472938.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+            </div>
+            <h2>Fisica</h2>
+            </div>
+            <div className="w-3/12 h-2/4 bg-red-900 m-2">
+            <div>
+            <img className="object-fill"
+            alt="cience" 
+            src="https://images.pexels.com/photos/7054413/pexels-photo-7054413.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+            </div>
+            <h2>Economia</h2>
+            </div>
+            <div className="w-3/12 h-2/4 bg-red-900 m-2">
+            <div>
+            <img className="object-contain h-2/4"
+            alt="cience" 
+            src="https://images.pexels.com/photos/3825527/pexels-photo-3825527.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"></img>
+            </div>
+            <h2>Quimica</h2>
+            </div>
+              
+            </div>
+            
         </div>
     </div> );
 }
